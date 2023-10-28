fd506cf93b4e8de7b402dd28bd7c6d026a9379f0
crook-ed <rajatgoyal6404@gmail.com>
2023-10-28 06:53:26 +0530

    set up frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
-import React , {Fragment} from 'react';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
-function App(){
-    return (
-        <Fragment></Fragment>
-    );
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Routes,
+} from "react-router-dom";
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
