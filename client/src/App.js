705ff93d2ffc0ae4b023e956adf355b502afaf5e
crook-ed <rajatgoyal6404@gmail.com>
2023-10-28 16:36:22 +0530

    added is-verify route tot he client

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
 import Dashboard from "./components/Dashboard";
@@ -19,6 +19,26 @@ function App() {
     const setAuth = (boolean) => {
         setIsAuthenticated(boolean);
     }
+
+    async function isAuth() {
+        try {
+            const response = await fetch("http://localhost:5000/auth/is-verify", {
+            method: "GET",
+            headers: {  token: localStorage.token }
+          });
+          
+          const parseRes = await response.json();
+
+          parseRes === true ? setIsAuthenticated(true): setIsAuthenticated(false);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    useEffect (() => {
+        isAuth();
+    })
+
 
     return (
         <>
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -26,7 +26,7 @@ const Dashboard = ({setAuth}) => {
 
       useEffect(() => {
         getProfile();
-      });
+      }, []);
 
     return (
         <>
